ffe01b45337df0092300c98eba6fd6e9c31594c8
wuyu0107 <mkim050107@gmail.com>
2024-09-24 23:13:31 +0700

    added cookies and login

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h44*@w842%ule_w-r5pd!_l*_bwzv5iw2f1vn+$a$d^-pfw#v$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "min-kim-ecommerce.pbp.cs.ui.ac.id"]
 
--- main/views.py
+++ main/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from main.forms import NotesEntryForm
 from main.models import Product
-from django.http import HttpResponse
+import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    notes_entries = Product.objects.all()
+    notes_entries = Product.objects.filter(user=request.user)
     
     context = {
-        'name' : 'Min Kim',
+        'name' : request.user.username,
         'class': 'KKI',
-        'notes_entries' : notes_entries
+        'notes_entries' : notes_entries,
+        'last_login' : request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -20,7 +28,9 @@ def create_note_entry(request):
     form = NotesEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        note_entry = form.save(commit=False)
+        note_entry.user = request.user
+        note_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -41,3 +51,37 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
